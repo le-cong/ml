477c08689ea53d15b0db3c618fe51496e5497c0e
Le Cong <le.cong@onestepride.com>
2020-05-08 01:48:17 -0400

    ex5 init

--- ex4/ex4.m
+++ ex4/ex4.m
@@ -186,7 +186,7 @@ fprintf('\nTraining Neural Network... \n')
 options = optimset('MaxIter', 50);
 
 %  You should also try different values of lambda
-lambda = 4;
+lambda = 1;
 
 % Create "short hand" for the cost function to be minimized
 costFunction = @(p) nnCostFunction(p, ...
